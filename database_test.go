4702c6bd1c646a909a9cc838ddb5cc63d41c886d
lotttotto <kentaro.saito50@gmail.com>
2020-10-17 14:47:13 +0900

    add test

--- database_test.go
+++ database_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	_ "github.com/lib/pq"
@@ -14,6 +15,14 @@ func TestDataBaseIntegrate(t *testing.T) {
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/list", nil)
 	router.ServeHTTP(w, req)
-
 	assert.Equal(t, 200, w.Code)
+}
+
+func TestDatabasecannotConnect(t *testing.T) {
+	os.Setenv("DbHost", "aaaaaaaa")
+	router := setupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/list", nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 500, w.Code)
 }
